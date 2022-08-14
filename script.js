2eb4417c9cd884b1ea85246c2c73b7fa7f93c3a9
Krzysztof <ki.zwolak@gmail.com>
2022-08-14 03:18:09 +0200

    Solved styling problems

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-function sixteen() {
-    for (i = 0; i < 64; i++) {
+let squares = prompt('How many squares, boss?');
+if (squares > 100) {
+    alert('Not that much, boss!')
+}
+
+
+const container = document.querySelector(".container")
+container.style.setProperty('grid-template-columns', 'repeat(' + squares + ', auto)');
+
+for (i = 0; i < squares; i++) {
         let row = document.createElement("div");
         row.className = "row";
-        for (j = 0; j < 64; j++) {
+        for (j = 0; j < squares; j++) {
             let box = document.createElement("div");
             box.className = "box";
             row.appendChild(box);
@@ -10,24 +18,20 @@ function sixteen() {
                 box.classList.add('newbox');
             }
             box.addEventListener('mousedown', colour);
-
-            let reset = document.querySelector("#reset");
-
-            let allBoxes = document.querySelector(".container").querySelectorAll(".box")
-            
-            function clear() {
-                allBoxes.forEach(box => {
-                    box.classList.remove('newbox');
-                })
-            }
-
-            reset.addEventListener('click', clear);
-
         }
         document.querySelector(".container").appendChild(row);
-    }
-
 }
 
-sixteen();
+let reset = document.querySelector("#reset");
+
+let allBoxes = document.querySelector(".container").querySelectorAll(".box")
+
+function clear() {
+    allBoxes.forEach(box => {
+        box.classList.remove('newbox');
+    })
+}
+
+reset.addEventListener('click', clear);
+
 
