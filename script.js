15314a448898b979ba39dff3630e69f20aaeda73
Krzysztof <ki.zwolak@gmail.com>
2022-08-14 05:41:56 +0200

    Add basic reset button

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-let squares = prompt('How many squares, boss?');
-if (squares > 100) {
-    alert('Not that much, boss!')
+function ask() {
+    let squares = prompt("How big do you want it, boss?");
+    if (squares > 100) {
+        alert("That's too large, boss!");
+        return ask();
+    }
+    else if (squares <= 0) {
+        alert("That's too little, boss!");
+        return ask();
+    }
+    else {
+        return squares;
+    }
 }
 
+let squares = ask();
 
 const container = document.querySelector(".container")
 container.style.setProperty('grid-template-columns', 'repeat(' + squares + ', auto)');
@@ -24,7 +35,9 @@ for (i = 0; i < squares; i++) {
 
 let reset = document.querySelector("#reset");
 
-let allBoxes = document.querySelector(".container").querySelectorAll(".box")
+let allBoxes = document.querySelector(".container").querySelectorAll(".box");
+
+let allRows = document.querySelector(".container").querySelectorAll(".row");
 
 function clear() {
     allBoxes.forEach(box => {
@@ -34,4 +47,34 @@ function clear() {
 
 reset.addEventListener('click', clear);
 
+function makeNew() {
+    allRows.forEach(row => {
+        row.remove();
+    })
+    allBoxes.forEach(box => {
+        box.remove();
+    })
 
+    let squares = ask();
+
+    container.style.setProperty('grid-template-columns', 'repeat(' + squares + ', auto)');
+
+    for (i = 0; i < squares; i++) {
+        let row = document.createElement("div");
+        row.className = "row";
+        for (j = 0; j < squares; j++) {
+            let box = document.createElement("div");
+            box.className = "box";
+            row.appendChild(box);
+            function colour() {
+                box.classList.add('newbox');
+            }
+            box.addEventListener('mousedown', colour);
+        }
+        document.querySelector(".container").appendChild(row);
+    }
+}
+
+let newGrid = document.querySelector('#newgrid');
+
+newGrid.addEventListener('mousedown', makeNew);
